805ff1ab0b36e215b2182ccdbf40dc61e5b3bb20
pm-rahman <p.m.rahaman0181@gmail.com>
2024-07-12 02:49:10 +0600

    refactor sidebar

--- src/app/(dasboard)/layout.js
+++ src/app/(dasboard)/layout.js
@@ -1,14 +1,25 @@
+"use client";
 import Header from "@/components/Header";
 import Sidebar from "../../components/Sidebar";
 import React from "react";
 import "../globals.css";
 
 export default function DashboardLayout({ children }) {
+  const [sidebarOpen, setSidebarOpen] = React.useState(true);
+  const [mobileSidebarOpen, setMobileSidebarOpen] = React.useState(false);
+  const [isProfileMenuOpen, setIsProfileMenuOpen] = React.useState(false);
   return (
     <div className="flex">
-      <Sidebar />
+      <Sidebar
+        sidebarOpen={sidebarOpen}
+        setSidebarOpen={setSidebarOpen}
+        mobileSidebarOpen={mobileSidebarOpen}
+        setMobileSidebarOpen={setMobileSidebarOpen}
+        isProfileMenuOpen={isProfileMenuOpen}
+        setIsProfileMenuOpen={setIsProfileMenuOpen}
+      />
       <main className="flex-1">
-        <Header />
+        <Header setMobileSidebarOpen={setMobileSidebarOpen} />
         <div className="lg:p-4 bg-slate-50 min-h-screen">{children}</div>
       </main>
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,58 +1,76 @@
 "use client";
-import { ChevronDownIcon, Search } from "lucide-react";
+import {
+  Bell, Menu,
+  MessageSquareWarning,
+  Search
+} from "lucide-react";
 import Image from "next/image";
+import { useForm } from "react-hook-form";
 import { Form, FormControl, FormField, FormItem } from "./ui/form";
 import { Input } from "./ui/input";
-import { useForm } from "react-hook-form";
 
-export default function Header({}) {
+export default function Header({ setMobileSidebarOpen }) {
   const form = useForm();
   const { handleSubmit, reset } = form;
   const onSubmit = async (data) => {
     console.log("data", data);
   };
   return (
-    <div className="pl-16 lg:pl-4 flex justify-between p-4 items-center gap-2">
-      <Form {...form}>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <FormField
-            control={form.control}
-            name="search"
-            render={({ field }) => (
-              <FormItem className="ml-4 bg-slate-100 rounded-md space-y-0 flex py-0 px-4 items-center">
-                <Search className="size-6 stroke-slate-400" />
-                <FormControl className="">
-                  <Input
-                    {...field}
-                    placeholder="Search"
-                    className="bg-transparent w-[300px] border-none focus-visible:ring-0 focus-visible:ring-offset-0"
-                  />
-                </FormControl>
-              </FormItem>
-            )}
+    <div className="flex justify-between p-4 items-center gap-2">
+      <div className="flex items-center gap-4 sm:gap-6">
+        {/* Mobile open menu bar */}
+        <div className="lg:hidden flex items-center gap-4 sm:gap-6">
+          <div onClick={() => setMobileSidebarOpen(true)}>
+            <Menu className="size-6 stroke-slate-500" />
+          </div>
+          <div className="h-10 border-r" />
+        </div>
+        <Form {...form}>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <FormField
+              control={form.control}
+              name="search"
+              render={({ field }) => (
+                <FormItem className="lg:ml-4 bg-slate-100 rounded-md space-y-0 flex py-0 px-4 items-center">
+                  <Search className="size-6 stroke-slate-400" />
+                  <FormControl className="">
+                    <Input
+                      {...field}
+                      placeholder="Search"
+                      className="bg-transparent sm:w-[300px] border-none focus-visible:ring-0 focus-visible:ring-offset-0"
+                    />
+                  </FormControl>
+                </FormItem>
+              )}
+            />
+          </form>
+        </Form>
+      </div>
+      {/* Right side */}
+      <div className="flex gap-3 sm:gap-10 xl:gap-12 items-center">
+        <div className="flex gap-2 sm:gap-4 lg:gap-5 items-center">
+          <MessageSquareWarning className="size-5 stroke-slate-500" />
+          <Bell className="size-5 stroke-slate-500" />
+        </div>
+
+        <div className="relative flex gap-1 sm:gap-2 items-center">
+          <div className="hidden lg:flex lg:items-center">
+            <div
+              aria-hidden="true"
+              className="text-sm flex flex-col text-gray-900 text-end"
+            >
+              <span className="font-semibold">Tom Cook</span>
+              <span>Admin Manager</span>
+            </div>
+          </div>
+          <Image
+            alt=""
+            width={40}
+            height={40}
+            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+            className="h-8 w-8 rounded-full bg-gray-50"
           />
-        </form>
-      </Form>
-      <div className="flex">
-        <Image
-          alt=""
-          width={40}
-          height={40}
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-          className="h-8 w-8 rounded-full bg-gray-50"
-        />
-        <span className="hidden lg:flex lg:items-center">
-          <span
-            aria-hidden="true"
-            className="ml-4 text-sm font-semibold leading-6 text-gray-900"
-          >
-            Tom Cook
-          </span>
-          <ChevronDownIcon
-            aria-hidden="true"
-            className="ml-2 h-5 w-5 text-gray-400"
-          />
-        </span>
+        </div>
       </div>
     </div>
   );
